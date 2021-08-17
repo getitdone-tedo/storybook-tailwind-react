99c408c36423b12a60cd92c50998d42ca737f175
Tedo <getitdonetedo@gmail.com>
2021-08-17 10:10:09 -0700

    project on progress

--- src/components/Tailwind/TButton.stories.tsx
+++ src/components/Tailwind/TButton.stories.tsx
@@ -6,11 +6,7 @@ export default {
 title: 'Tailwind/TButton',
 component: TButton,
 argTypes: {
-    variant: { 
-        name: 'variant',
-        type: { name: 'string', required: false },
-        control: { type: 'string' } 
-    },
+    backgroundColor: { control: 'color' },
 },
 } as ComponentMeta<typeof TButton>;
 
@@ -21,11 +17,41 @@ export const Primary = Template.bind({});
 
 Primary.args = {
 variant: 'primary',
-children: "Primary Button"
+label: "Primary Button",
+backgroundColor: "bg-red-600 hover:bg-indigo-700"
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
     variant: 'secondary',
-    children: "Secondary Button"
+    label: "Secondary Button"
+}
+
+export const Danger = Template.bind({});
+Danger.args = {
+    variant: 'danger',
+    label: "Danger Button"
+}
+
+export const Small = Template.bind({});
+Small.args = {
+    variant: 'primary',
+    label: "Small Button",
+    size: "px-4 py-4",
+    backgroundColor: "bg-red-600 hover:bg-indigo-700"
+}
+
+export const Medium = Template.bind({});
+Medium.args = {
+    variant: 'primary',
+    label: "Medium Button",
+    size: "px-8 py-8",
+    backgroundColor: "bg-red-600 hover:bg-indigo-700"
+}
+export const Large = Template.bind({});
+Large.args = {
+    variant: 'primary',
+    label: "Large Button",
+    size: "px-16 py-16",
+    backgroundColor: "bg-red-600 hover:bg-indigo-700"
 }
--- src/components/Tailwind/TButton.tsx
+++ src/components/Tailwind/TButton.tsx
@@ -1,19 +1,61 @@
 import classNames from 'classnames';
-import React from 'react'
+import React from 'react';
+
+
 
 const buttonClasses = (variant: any) => 
     classNames({
-        'border-transparent text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500': !variant || variant === 'primary',
+        'border-transparent text-white focus:ring-indigo-500': !variant || variant === 'primary',
         'border-transparent text-white bg-red-600 focus:ring-red-700 focus:border-red-600': variant === 'danger-primary',
         'border-grey-300 text-gray-700 bg-white hover:bg-gray-50 focus:ring-indigo-500': variant === 'secondary',
         'border-red-600 text-red-600 bg-white focus:ring-red-600 focus:border-red-600': variant === 'danger',
         'inline-flex items-center px-4 py-2 border rounded-md shadow-sm text-sm font-medium focus:outline-node focus:ring-2 focus:ring-offset-2 ': true
     });
 
-const TButton = (props: any) => {
-    const {  variant, children, onClick, ...rest } = props;
+// const buttonSizes = (size: any) => 
+//     classNames({
+//         'px-2 py-2': !size || size === 'small',
+//         'px-4 py-4': size === 'medium',
+//         'px-6 py-6': size === 'large',
+//     });
+
+
+interface ButtonProps {
+    /**
+     * Is this the principal call to action on the page?
+     */
+    variant?: 'primary' | 'secondary' | 'danger' | 'danger-primary';
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
+    /**
+     * How large should the button be?
+     */
+    // size?: 'small' | 'medium' | 'large';
+    size?: string;
+    /**
+     * Button contents
+     */
+    label: string;
+    /**
+     * Optional click handler
+     */
+    onClick?: () => void;
+}  
+
+
+const TButton = ({
+    variant = 'primary',    
+    size,
+    backgroundColor,
+    label,
+  ...props
+}: ButtonProps) => {
     return (
-        <button className={buttonClasses(variant)} onClick={onClick} {...rest}>{children}</button>
+        <button className={[buttonClasses(variant), size, backgroundColor].join(' ')} {...props}>
+            {label}
+        </button>
     );
 };
 
