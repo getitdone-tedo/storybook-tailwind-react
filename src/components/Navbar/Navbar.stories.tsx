d814ba375ea030bf75c99b813c8283ab182507cb
Tedo <getitdonetedo@gmail.com>
2021-08-19 10:59:45 -0700

    navbar

---
+++ src/components/Navbar/Navbar.stories.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+    title: 'Components/UI Elements/Navbar',
+    component: Navbar,
+    // argTypes: {
+    //     size: {control: 'text'}
+    // }
+} as ComponentMeta<typeof Navbar>;
